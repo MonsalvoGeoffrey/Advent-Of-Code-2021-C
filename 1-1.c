fdbb5f230fd255269336dfed6c1970e13fcfb424
Monsalvo Geoffrey <monsalvo.geoffrey@gmail.com>
2023-11-20 17:26:38 +0100

    first commit

---
+++ 1-1.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef char string[];
+typedef unsigned char uint8;
+typedef signed char int8;
+typedef unsigned short uint16;
+typedef signed short int16;
+typedef unsigned long uint32;
+typedef signed long int32;
+typedef unsigned long long uint64;
+typedef signed long long int64;
+
+#define println(format, ...) printf(format "\n", ##__VA_ARGS__)
+
+int main()
+{
+    FILE *input = fopen("1-1.txt", "r");
+    char line[255];
+    uint16 last = 0;
+    uint32 increases = 0;
+
+    while (fgets(line, 255, input) != NULL)
+    {
+        uint16 current = atoi(line);
+        if (current > last)
+        {
+            increases++;
+        }
+        last = current;
+    }
+
+    if (increases > 0)
+    {
+        increases--;
+    }
+    fclose(input);
+    println("Total Increases: %d", increases);
+
+    return 0;
+}
